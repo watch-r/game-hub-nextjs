40cd8eb711be270520d8fca48c7e6ef37a359f3e
Watch <dwatchr.man@gmail.com>
2024-01-27 20:26:33 +0600

    Displaying Critic Score

---
+++ components/CriticScore.tsx
@@ -0,0 +1,21 @@
+import { Badge } from "@radix-ui/themes";
+
+const CriticScore = ({ score }: { score: number }) => {
+    const color =
+        score > 90
+            ? "green"
+            : score > 60
+            ? "orange"
+            : score > 50
+            ? "yellow"
+            : score > 40
+            ? "red"
+            : "cyan";
+    return (
+        <Badge variant='surface' color={color} size={"1"}>
+            {score}
+        </Badge>
+    );
+};
+
+export default CriticScore;
--- components/GameCard.tsx
+++ components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import Image from "next/image";
 import React from "react";
 import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 
 interface Props {
     game: Game;
@@ -14,22 +15,27 @@ const GameCard = ({ game }: Props) => {
     };
     return (
         <Card style={{ maxWidth: 400 }}>
-            <Flex direction={"column"} gap={"2"} align={'center'}>
-                <Image
-                    objectFit="contain"
-                    loader={myLoader}
-                    src={game.background_image}
-                    alt=''
-                    width={"300"}
-                    height={"300"}
-                    className='rounded-md'
-                />
-                <Heading size={"5"} weight={"medium"}>
-                    {game.name}
-                </Heading>
-                <PlatformIconList
-                    platforms={game.parent_platforms.map((p) => p.platform)}
-                />
+            <Flex direction={"column-reverse"} gap={"2"} justify={"between"}>
+                <Flex direction={"column"} align={"center"}>
+                    <Image
+                        objectFit='contain'
+                        loader={myLoader}
+                        src={game.background_image}
+                        alt=''
+                        width={"300"}
+                        height={"300"}
+                        className='rounded-md'
+                    />
+                    <Heading size={"5"} weight={"medium"} className="p-3">
+                        {game.name}
+                    </Heading>
+                </Flex>
+                <Flex justify={"between"}>
+                    <PlatformIconList
+                        platforms={game.parent_platforms.map((p) => p.platform)}
+                    />
+                    <CriticScore score={game.metacritic} />
+                </Flex>
             </Flex>
         </Card>
     );
