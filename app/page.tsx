188b07e7f7dfa17a718b1732478643cea06dda27
Watch <dwatchr.man@gmail.com>
2024-02-03 20:36:04 +0600

    Courasal

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,13 @@
-"use client";
-import SearchInput from "@/components/SearchInput";
-import { Button as Botton } from "@/components/ui/button";
-import { Button, Container, Flex, Select } from "@radix-ui/themes";
+import GameCarousel from "@/components/GameCarousel";
+import { Container, Flex } from "@radix-ui/themes";
 
 export default function Home() {
-    // console.log(games)
     return (
         <>
             <Container>
-                <div>Hello World</div>
-                <Flex direction='column' gap={"3"} width={"min-content"}>
-                    <Button>Button radix ui check</Button>
-                    <Botton>Button shadcn ui check</Botton>
+                <Flex direction='column' align='center'>
+                    <div>Hello World</div>
+                    <GameCarousel />
                 </Flex>
             </Container>
         </>
